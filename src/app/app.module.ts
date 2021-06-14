aa01af2126b38f823b74a0d82240c750eff41b76
Diego Torres <diegotc55@gmail.com>
2021-06-14 11:15:20 -0500

    pokemon module create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+//Modulos
+import { PokemonModule } from "./pokemon/pokemon.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PokemonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PokemonlistComponent } from './pokemonlist/pokemonlist.component';
+
+
+
+@NgModule({
+  declarations: [
+    PokemonlistComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonModule { }
