27429d73f14a5e1335dc1ce13ed0459e8386dbd8
Diego Torres <diegotc55@gmail.com>
2021-06-15 22:02:30 -0500

    Add Interfaces and get all pokemon from pokeapi

--- src/app/pokemon/pokemonlist/pokemonlist.component.ts
+++ src/app/pokemon/pokemonlist/pokemonlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PokemonService } from '../services/pokemon.service';
+import { Pokemon } from "../interfaces/pokemon.interfaces";
 
 @Component({
   selector: 'app-pokemonlist',
@@ -7,13 +8,16 @@ import { PokemonService } from '../services/pokemon.service';
   styleUrls: ['./pokemonlist.component.css']
 })
 export class PokemonlistComponent implements OnInit {
-
+  
+  public pokemons: Pokemon[] = [];
+  
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
-    this.pokemonService.getAllPokemons().subscribe(resp =>{
-      console.log(resp);
-    });
+    this.pokemonService.getAllPokemons()
+      .subscribe(pokemons => {
+        this.pokemons = pokemons;
+      });
   }
 
 }
--- src/app/pokemon/services/pokemon.service.ts
+++ src/app/pokemon/services/pokemon.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { map } from "rxjs/operators";
+
+import { FetchAllPokemonResponse, Pokemon } from '../interfaces/pokemon.interfaces';
+import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +15,27 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-  getAllPokemons() {
-    return this.http.get(`${this.url}/pokemon/?limit=1500`)
+  getAllPokemons(): Observable<Pokemon[]> {
+    return this.http.get<FetchAllPokemonResponse>(`${this.url}/pokemon/?limit=1500`)
+      .pipe(
+        map(this.transformSmallPokemonIntoPokemon)
+      )
   }
+  private transformSmallPokemonIntoPokemon(resp: FetchAllPokemonResponse): Pokemon[] {
 
+    const pokemonList: Pokemon[] = resp.results.map(poke => {
+
+      const urlArr = poke.url.split('/');
+      const id = urlArr[6];
+      const pic = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+
+      return {
+        id,
+        pic,
+        name: poke.name
+      }
+    }
+    )
+    return pokemonList;
+  }
 }
